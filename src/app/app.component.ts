5708dced491def5085151ff2f110e00287c5c8c5
malikrajat <er.rajatmalik@gmail.com>
2022-01-26 13:32:00 +0530

    service worker impl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwPush, SwUpdate} from "@angular/service-worker";
+import {DataService} from "./data.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'jokes';
+  update!: boolean;
+  joke!: any;
+  constructor(private swUpdate: SwUpdate, private data : DataService, readonly swPush: SwPush) {
+  }
+
+  ngOnInit() {
+    this.reloadCache();
+    this.data.getJokes().subscribe(res=> {
+      this.joke = res;
+    })
+  }
+
+  reloadCache(){
+    if(this.swUpdate.isEnabled){
+      this.swUpdate.available.subscribe(() =>{
+        if(confirm('New version available! would you like to update?')){
+          this.swUpdate.activateUpdate().then(()=> document.location.reload());
+        }
+      })
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import {HttpClientModule} from "@angular/common/http";
+import {DataService} from "./data.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [
+    DataService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
